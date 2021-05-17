d3412ad72fa84d9e99eb3c392758d47dc93ae861
unknown <diovanlucas0@gmail.com>
2021-05-17 17:02:49 -0300

    primeiro commit

---
+++ etapas.js
@@ -0,0 +1,60 @@
+let etapas = [
+    {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+        {
+            numero: "11111",
+            nome:"Gon",
+            partido: "Freecs",
+            fotos:[
+                {url:"0gon.jpg", legenda:"Vereador", medium:true}
+            ]
+        },
+        {
+            numero: "22222",
+            nome:"Killua",
+            partido: "Zoldyck",
+            fotos:[
+                {url:"0killua.jpg", legenda:"Vereador",medium:true}
+            ]
+        },
+    ]
+ },
+ {
+    titulo: "Prefeito",
+    numeros: 2,
+    candidatos:[
+        {
+            numero: "11",
+            nome:"Luffy",
+            partido: "Pirata",
+            vice: 'Zoro',
+            fotos:[
+                {url:"1Luffy.jpg", legenda:"Prefeito", small: false},
+                {url:"1zoro.jpg", legenda:"Vice-Prefeito", small: true}
+            ]
+        },
+        {
+            numero: "21",
+            nome:"Kageyama",
+            partido: "Atleta",
+            vice: 'Hinata',
+            fotos:[
+                {url:"2Kageyama.jpg", legenda:"Prefeito", small: false},
+                {url:"2hinata.jpg", legenda:"Vice-Prefeito", small: true}
+            ]
+        },
+        {
+            numero: "31",
+            nome:"Tanjiro",
+            partido: "Hashira",
+            vice: 'Nezuko',
+            fotos:[
+                {url:"3Tanjiro.jpg", legenda:"Prefeito", small: false},
+                {url:"3Nezuko.jpg", legenda:"Vice-Prefeito", small: true}
+            ]
+        },
+    ]
+}
+];
